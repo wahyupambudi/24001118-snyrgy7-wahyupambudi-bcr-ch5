2b1450cf2bcdd682994da655f86a20b1aa047ee6
Wahyu Pambudi <wahyupambudi823@gmail.com>
2024-05-19 21:07:49 +0700

    feat(bcr) - add CRUD for Categories

--- src/handlers/categories.handler.ts
+++ src/handlers/categories.handler.ts
@@ -10,7 +10,82 @@ const handleListCategories = async (_req: Request, res: Response) => {
     })
 }
 
+const handleCategorieById = async (req: Request, res: Response) => {
+    const id: Number = Number(req.params.id);
+    try {
+        const categorie = await CategoriesModel.query().findById(Number(id)).throwIfNotFound();
+
+        res.status(200).json({
+            message: "Success",
+            categorie
+        })
+        return;
+    } catch (err) {
+        res.status(404).json({
+            message: `${err}`
+        })
+        return;
+    }
+}
+
+const handleCreateCategorie = async (req: Request, res: Response) => {
+    const getBody = req.body;
+    try {
+        const categorie = await CategoriesModel.query().insert(getBody);
+        res.status(201).json({
+            message: "Success",
+            categorie
+        })
+        return;
+    } catch (err) {
+        console.log(err)
+        res.status(400).json({
+            message: "Data Is Not Null!"
+        })
+        return;
+    }
+}
+
+const handlePutCategorie = async (req: Request, res: Response) => {
+    const id: Number = Number(req.params.id);
+    const getBody = req.body;
+    try {
+        await CategoriesModel.query().findById(Number(id)).throwIfNotFound().patch(getBody)
+        const categories = await CategoriesModel.query().orderBy('id');
+        res.status(200).json({
+            message: "Success",
+            categories
+        })
+        return;
+    } catch (err) {
+        res.status(404).json({
+            message: `${err}`
+        })
+        return;
+    }
+}
+const handleDeleteCategorie = async (req: Request, res: Response) => {
+    const id: Number = Number(req.params.id);
+    try {
+        await CategoriesModel.query().deleteById(Number(id)).throwIfNotFound();
+        const categories = await CategoriesModel.query().orderBy('id');
+        res.status(200).json({
+            message: "Success",
+            categories
+        })
+        return;
+    } catch (err) {
+        res.status(404).json({
+            message: `${err}`
+        })
+        return;
+    }
+}
 
 module.exports = {
-    handleListCategories
+    handleListCategories,
+    handleCategorieById,
+    handleCreateCategorie,
+    handlePutCategorie,
+    handleDeleteCategorie
 }
--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,9 +1,19 @@
-import {Router, Request, Response} from "express";
-const {handleListCategories} = require("../handlers/categories.handler")
+import {Router} from "express";
+const {
+    handleListCategories,
+    handleCategorieById,
+    handleCreateCategorie,
+    handlePutCategorie,
+    handleDeleteCategorie
+} = require("../handlers/categories.handler")
 
 const router = Router();
 
 // method Get
-router.get("/", handleListCategories)
+router.get("/", handleListCategories);
+router.get("/:id", handleCategorieById);
+router.post("/add", handleCreateCategorie);
+router.put("/update/:id", handlePutCategorie);
+router.delete("/:id", handleDeleteCategorie);
 
 export default router;
